8412c23ff4ee81606d8433f8daec2b815a7895c8
tzuleger <74215332+tzuleger@users.noreply.github.com>
2021-11-02 14:20:52 -0500

    Added feedback function to GetAllAsync

--- AtriumREST/AtriumREST/AtriumController.cs
+++ AtriumREST/AtriumREST/AtriumController.cs
@@ -1014,8 +1014,9 @@ namespace ThreeRiversTech.Zuleger.Atrium.REST
         /// </summary>
         /// <typeparam name="T">Type of AtriumObject to grab from the Atrium Controller.</typeparam>
         /// <param name="fragmentSize">Size of how many records the function should grab per HTTP packet.</param>
+        /// <param name="feedback">Void type function that is used to create feedback to the User while this function executes.</param>
         /// <returns>List of Atrium Objects that are in the Controller.</returns>
-        public async Task<List<T>> GetAllAsync<T>(int fragmentSize = -1) where T : AtriumObject, new()
+        public async Task<List<T>> GetAllAsync<T>(int fragmentSize = -1, Action feedback=null) where T : AtriumObject, new()
         {
             var temp = FragmentSize;
             FragmentSize = fragmentSize >= 25 ? fragmentSize : FragmentSize;
@@ -1032,6 +1033,7 @@ namespace ThreeRiversTech.Zuleger.Atrium.REST
                 os = os.Concat(grab).ToList();
                 sIdx = eIdx + 1;
                 eIdx += FragmentSize;
+                feedback?.Invoke();
             }
 
             FragmentSize = temp;
