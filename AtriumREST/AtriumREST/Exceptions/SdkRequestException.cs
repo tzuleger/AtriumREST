2daba170d17950cecb6452806530f69a486121df
tzuleger <74215332+tzuleger@users.noreply.github.com>
2021-11-11 15:29:02 -0600

    Added SdkError to SdkRequestException

--- AtriumREST/AtriumREST/Exceptions/SdkRequestException.cs
+++ AtriumREST/AtriumREST/Exceptions/SdkRequestException.cs
@@ -8,10 +8,20 @@ namespace ThreeRiversTech.Zuleger.Atrium.REST.Exceptions
     public class SdkRequestException : Exception
     {
         /// <summary>
+        /// Error message from an XML answer that states that the Object being inserted already exists with some unique value.
+        /// </summary>
+        public static string ERR_ALREADY_EXIST = "err_already_exist";
+
+        /// <summary>
         /// Message to be printed when thrown.
         /// </summary>
         public override String Message { get => _msg; }
         private String _msg;
+        /// <summary>
+        /// Error Code from the XML answer.
+        /// </summary>
+        public String SdkError { get => _sdkError; set => _sdkError = value; }
+        private String _sdkError;
 
         /// <summary>
         /// Thrown when an SDK Request returns back with err not equaling "ok".
@@ -24,6 +34,7 @@ namespace ThreeRiversTech.Zuleger.Atrium.REST.Exceptions
         /// <param name="errcode">Error Code that was given instead of "ok"</param>
         public SdkRequestException(String errcode)
         {
+            this.SdkError = errcode;
             _msg = $"Controller responded with an unsuccessful status code: {errcode}.";
         }
     }
